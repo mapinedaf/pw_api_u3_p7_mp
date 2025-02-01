35f84d40fb497b7f928fe9ec7b4af15ab089d1f2
mapinedaf <mapinedaf@uce.edu.ec>
2025-02-01 00:44:48 +0000

    taller-29

--- src/main/java/uce/edu/web/api/controller/PersonaController.java
+++ src/main/java/uce/edu/web/api/controller/PersonaController.java
@@ -1,7 +1,11 @@
 package uce.edu.web.api.controller;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 import uce.edu.web.api.service.IPersonaService;
@@ -20,18 +24,30 @@ public class PersonaController {
         return Response.ok(this.personaService.buscarPorId(id)).build();
     }
 
+    @POST
     @Path("/guardar")
     public void guardar(PersonaTo persona) {
         this.personaService.guardar(persona);;
     }
 
+    @PUT
     @Path("/actualizar")
     public void actualizar(PersonaTo persona) {
        this.personaService.actualizar(persona);;
     }
 
+    @PATCH
+    @Path("/actualizar/parcial")
+    public void actualizarParcial(PersonaTo persona) {
+    PersonaTo tmp = this.personaService.buscarPorId(persona.getId());
+    tmp.setNombre(persona.getNombre());
+       this.personaService.actualizar(tmp);
+    }
+
+    @DELETE
     @Path("/borrar")
-    public void borrar(Integer id) {
+    public void borrar() {
+        Integer id =1;
         this.personaService.borrar(id);;
     }
 
