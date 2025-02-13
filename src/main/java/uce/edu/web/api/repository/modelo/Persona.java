f87db0753490828aed7f5cf35548a17fa895d3e5
mapinedaf <mapinedaf@uce.edu.ec>
2025-02-13 21:24:01 +0000

    tarea-12

--- src/main/java/uce/edu/web/api/repository/modelo/Persona.java
+++ src/main/java/uce/edu/web/api/repository/modelo/Persona.java
@@ -9,11 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name ="persona")
